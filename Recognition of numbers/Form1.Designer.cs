c1ba4777d299b3b0f045140b3ab64896b622af15
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2018-04-28 13:26:33 +0200

    First commit

---
+++ Recognition of numbers/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Recognition_of_numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDrawing = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBoxTeaching = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton0 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.groupBoxTeaching.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDrawing
+            // 
+            this.panelDrawing.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelDrawing.Location = new System.Drawing.Point(12, 12);
+            this.panelDrawing.Name = "panelDrawing";
+            this.panelDrawing.Size = new System.Drawing.Size(300, 300);
+            this.panelDrawing.TabIndex = 0;
+            this.panelDrawing.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDrawing_Paint);
+            this.panelDrawing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelDrawing_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBox1.Location = new System.Drawing.Point(331, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(194, 300);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBoxTeaching
+            // 
+            this.groupBoxTeaching.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBoxTeaching.Controls.Add(this.button1);
+            this.groupBoxTeaching.Controls.Add(this.radioButton9);
+            this.groupBoxTeaching.Controls.Add(this.radioButton8);
+            this.groupBoxTeaching.Controls.Add(this.radioButton7);
+            this.groupBoxTeaching.Controls.Add(this.radioButton6);
+            this.groupBoxTeaching.Controls.Add(this.radioButton5);
+            this.groupBoxTeaching.Controls.Add(this.radioButton4);
+            this.groupBoxTeaching.Controls.Add(this.radioButton3);
+            this.groupBoxTeaching.Controls.Add(this.radioButton2);
+            this.groupBoxTeaching.Controls.Add(this.radioButton1);
+            this.groupBoxTeaching.Controls.Add(this.radioButton0);
+            this.groupBoxTeaching.Location = new System.Drawing.Point(12, 318);
+            this.groupBoxTeaching.Name = "groupBoxTeaching";
+            this.groupBoxTeaching.Size = new System.Drawing.Size(200, 120);
+            this.groupBoxTeaching.TabIndex = 2;
+            this.groupBoxTeaching.TabStop = false;
+            this.groupBoxTeaching.Text = "Teaching";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Teach";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonTeachClick);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(154, 55);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 9;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(117, 55);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(31, 17);
+            this.radioButton8.TabIndex = 8;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(80, 55);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 7;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(43, 55);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 55);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(154, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(117, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton0
+            // 
+            this.radioButton0.AutoSize = true;
+            this.radioButton0.Location = new System.Drawing.Point(6, 19);
+            this.radioButton0.Name = "radioButton0";
+            this.radioButton0.Size = new System.Drawing.Size(31, 17);
+            this.radioButton0.TabIndex = 0;
+            this.radioButton0.TabStop = true;
+            this.radioButton0.Text = "0";
+            this.radioButton0.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonCheck);
+            this.groupBox1.Controls.Add(this.buttonClean);
+            this.groupBox1.Location = new System.Drawing.Point(328, 318);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 120);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recognition";
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(63, 31);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheck.TabIndex = 1;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Location = new System.Drawing.Point(63, 80);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(75, 23);
+            this.buttonClean.TabIndex = 0;
+            this.buttonClean.Text = "Clean";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(544, 451);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxTeaching);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panelDrawing);
+            this.MinimumSize = new System.Drawing.Size(560, 490);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Recognition of numbers";
+            this.groupBoxTeaching.ResumeLayout(false);
+            this.groupBoxTeaching.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDrawing;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBoxTeaching;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton0;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonClean;
+    }
+}
+
---
+++ Recognition of numbers/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Recognition_of_numbers
+{
+    public partial class Form1 : Form
+    {
+     
+        Graphics graphic;
+        Pen penBlack;
+        Color penColor = new Color();
+
+        int[][] tabFromPanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            penColor = Color.FromArgb(0, 150, 50);
+
+            graphic = panelDrawing.CreateGraphics();
+
+            penBlack = new Pen(Color.Black, 1);
+
+
+            tabFromPanel = new int[10][];
+            for (int i = 0; i < 10; i++)
+            {
+                tabFromPanel[i] = new int[10];
+            }
+        }
+
+
+        private void panelDrawing_Paint(object sender, PaintEventArgs e)
+        {
+            for (float i = panelDrawing.Height / 10; i < panelDrawing.Height; i += panelDrawing.Height / 10)
+            {
+                graphic.DrawLine(penBlack, 0, i, panelDrawing.Width, i);
+
+            }
+            for (float i = panelDrawing.Width / 10; i < panelDrawing.Width; i += panelDrawing.Width / 10)
+            {
+                graphic.DrawLine(penBlack, i, 0, i, panelDrawing.Height);
+
+            }
+        }
+
+
+        private void SaveToTable()
+        {
+            Bitmap bit;
+            Graphics graph;
+            Rectangle rect;
+
+
+            if (textBox1.Text != null)
+            {
+                textBox1.Text = "";
+            }
+
+
+            bit = new Bitmap(panelDrawing.Width, panelDrawing.Height);
+            graph = Graphics.FromImage(bit);
+            rect = panelDrawing.RectangleToScreen(panelDrawing.ClientRectangle);
+            graph.CopyFromScreen(rect.Location, Point.Empty, panelDrawing.Size);
+
+            Color a = new Color();
+
+            int H = 15;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int W = 15;
+                for (int j = 0; j < 10; j++)
+                {
+                    a = bit.GetPixel(W, H);
+                    if (a == penColor)
+                    {
+                        tabFromPanel[i][j] = 1;
+                    }
+                    else
+                    {
+                        tabFromPanel[i][j] = 0;
+                    }
+
+                    
+                    int temp;
+                    temp = tabFromPanel[i][j];
+                    W += 30;
+                }
+                H += 30;
+            }
+
+           
+        }
+
+        private bool AllEmpty()
+        {
+            int emptyCounter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                   if(tabFromPanel[i][j]==0)
+                    {
+                        emptyCounter++;
+                    }
+                }
+            }
+
+            if (emptyCounter == 100)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private void panelDrawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            Brush b = null;
+
+            if (e.Button == MouseButtons.Left) b = new SolidBrush(penColor);
+            else if (e.Button == MouseButtons.Right) b = b = new SolidBrush(Color.WhiteSmoke);
+            if (b != null)
+            {
+                int w = 0, h = 0;
+
+
+                for (int i = 1; i < 11; i++)
+                {
+                    if (e.Y < (panelDrawing.Height / 10)) h = 1;
+                    else if (e.Y > (panelDrawing.Height / 10) * (i - 1) && e.Y < (panelDrawing.Height / 10) * i) h = i;
+                    if (e.X < (panelDrawing.Width / 10)) w = 1;
+                    else if (e.X > (panelDrawing.Width / 10) * (i - 1) && e.X < (panelDrawing.Width / 10) * i) w = i;
+                }
+
+                float w1, w2, h1, h2;
+                w1 = (panelDrawing.Width * (w - 1)) / 10;
+                w2 = panelDrawing.Width / 10;
+                h1 = (panelDrawing.Height * (h - 1)) / 10;
+                h2 = panelDrawing.Height / 10;
+
+                graphic.FillRectangle(b, w1 + 1, h1 + 1, w2 - 1, h2 - 1);
+                b = null;
+            }
+        }
+
+        private void buttonTeachClick(object sender, EventArgs e)
+        {
+  
+            int? whatNumber = null;
+
+            foreach (Control control in groupBoxTeaching.Controls)
+            {
+
+                if (control is RadioButton)
+                {
+                    RadioButton radio = control as RadioButton;
+                    if (radio.Checked)
+                    {
+                        whatNumber = Convert.ToInt32(control.Text);
+                    }
+                }
+            }
+
+            if(whatNumber ==null)
+            {
+                textBox1.Text = "Selct number!";
+                return;
+            }
+
+            string desktopPatch = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            desktopPatch += @"\Data number\";
+            if (!Directory.Exists(desktopPatch))
+            {
+                Directory.CreateDirectory(desktopPatch);
+            }
+
+            SaveToTable();
+
+            textBox1.Text = "Teaching complete!";
+
+            using (StreamWriter sw = new StreamWriter(desktopPatch + whatNumber + ".txt", true))
+            {
+
+                for (int i = 0; i < tabFromPanel.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tabFromPanel.GetLength(0); j++)
+                    {
+                        sw.Write(tabFromPanel[i][j]);
+                    }
+
+                    sw.WriteLine();
+                }
+            }
+
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            panelDrawing.BackgroundImage = null;
+            textBox1.Text = "";
+
+            panelDrawing.Refresh();
+
+            panelDrawing_Paint(null, null);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tabFromPanel[i][j] = 0;
+                }
+            } 
+       }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            SaveToTable();
+
+            if (AllEmpty())
+            {
+                textBox1.Text = "Empty Box!";
+                goto StopChecking;
+            }
+
+
+            string desktopPatch = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            desktopPatch += @"\Data number\";
+
+            if (!Directory.Exists(desktopPatch))
+            {
+                Directory.CreateDirectory(desktopPatch);
+            }
+
+
+            DirectoryInfo di = new DirectoryInfo(desktopPatch);
+            FileInfo[] fi = di.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            int[] chanceForNumber = new int[10];
+            int howManyNumbersInFile = 0;
+            int whichNumberNow = 0;
+
+            foreach (var file in fi)
+            {
+                int correctness = 0;
+                
+             
+
+                using (StreamReader sr = new StreamReader(file.FullName))
+                {
+
+
+                    int lineCount = File.ReadLines(file.FullName).Count();
+
+                    howManyNumbersInFile = lineCount / 10;
+
+
+                    int[][] localtab = new int[lineCount][];
+
+                    for (int i = 0; i < lineCount; i++)
+                    {
+                        localtab[i] = new int[10];
+                    }
+
+
+                    for (int i = 0; i < localtab.GetLength(0); i++)
+                    {
+                        string line = sr.ReadLine();
+
+                        for (int j = 0; j < 10; j++)
+                        {
+                            int symbol = line[j] == 48 ? 0 : 1;
+                            localtab[i][j] = symbol;
+                        }
+
+                    }
+
+                   
+                    
+
+                    int[] chance = new int[howManyNumbersInFile];
+
+                    int lineCounter = 0;
+
+                    for (int i = 0; i <howManyNumbersInFile; i++)
+                    {
+                       
+
+                        for (int j=0; j < tabFromPanel.GetLength(0); j++)
+                        {
+             
+                            for (int k = 0; k < tabFromPanel.GetLength(0); k++)
+                            {
+                                if (tabFromPanel[j][k] == localtab[lineCounter][k])
+                                {
+                                    correctness++;
+                                }
+                            }
+                            lineCounter++;
+                        }
+
+                        if(correctness>=95)
+                        {
+                            ShowResult(whichNumberNow, correctness);
+                            goto StopChecking;
+                        }
+
+                        chance[i] = correctness;
+                        correctness = 0;
+                    }
+
+
+                    chanceForNumber[whichNumberNow] = Convert.ToInt32(chance.Average());
+                    whichNumberNow++;
+                   
+                }
+
+            }
+
+           
+
+            int chanceForCorrect = chanceForNumber.Max();
+            int whatNumber = Array.IndexOf(chanceForNumber, chanceForCorrect);
+
+
+
+            ShowResult(whatNumber, chanceForCorrect);
+      
+
+            StopChecking:
+            {
+
+            }
+
+
+        }
+
+        void ShowResult(int whatNumber,int chanceForCorrect)
+        {
+            textBox1.Text = "Detected number " + whatNumber + " Chance " + chanceForCorrect + "%";
+        }
+    }
+}
